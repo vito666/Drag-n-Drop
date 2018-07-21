d04f770b8c9009402c35003b2a0c628f9f1a6739
Artem Konstantinov <don666vito@yandex.ua>
2018-07-21 20:49:25 +0300

    «Done»

---
+++ js/main.js
@@ -0,0 +1,47 @@
+const fill = document.querySelector('.fill');
+const empties = document.querySelectorAll('.empty');
+
+//Fill Listeners
+fill.addEventListener('dragstart', dragStart);
+fill.addEventListener('dragend', dragEnd);
+
+// Loop through empties and call drag events
+for(const empty of empties) {
+  empty.addEventListener('dragover', dragOver);
+  empty.addEventListener('dragenter', dragEnter);
+  empty.addEventListener('dragleave', dragLeave);
+  empty.addEventListener('drop', dragDrop);
+}
+
+// Drag Functions
+
+function dragStart() {
+  this.className += ' hold'
+ setTimeout(() => this.className = 'invisible', 0);
+}
+
+function dragEnd() {
+ this.className = 'fill';
+}
+
+function dragOver(e) {
+  e.preventDefault();
+  //console.log('over');
+}
+
+function dragEnter(e) {
+  e.preventDefault();
+  this.className += ' hovered';
+  //console.log('enter');
+}
+
+function dragLeave() {
+  this.className = 'empty';
+  //console.log('leave');
+}
+
+function dragDrop() {
+  this.className = 'empty';
+  this.append(fill);
+  //console.log('drop');
+}
